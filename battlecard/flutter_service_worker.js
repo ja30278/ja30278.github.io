d4053dc3421a9433f6713a011d61d87ea0894740
Jon Allie <jon@jonallie.com>
2023-03-19 08:03:54 -0400

    tweak text size, add per-unit points value

--- battlecard/flutter_service_worker.js
+++ battlecard/flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "940d24a8bf3cc8f4dcbe6262844cb771",
-"/": "940d24a8bf3cc8f4dcbe6262844cb771",
-"main.dart.js": "f4986091eaf030d1b84a0b56cfaeba8f",
+"index.html": "606dee060dbc3a9798c8c40908680538",
+"/": "606dee060dbc3a9798c8c40908680538",
+"main.dart.js": "340b4b634df851e4d5b78c48f1c78bb2",
 "manifest.json": "fbc82c43a1c1dbcd034b370191d64b64",
 "version.json": "33fbc240f2f4afe46c1b272a60151e50"
 };
